5bebf358a522f5c9878fa0b8fc53112bb683191c
bhargav katira <bhargavkatira@gmail.com>
2021-12-19 06:03:56 +0530

    hello

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ const app = require("./index")
 
 const connect = require("./configs/db")
 
-app.listen(3800, async function () {
+app.listen(3000, async function () {
 
     await connect()
 
-    console.log("listening to port 3800")
+    console.log("listening to port 3000")
 })
